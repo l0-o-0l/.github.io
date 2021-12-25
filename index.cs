d57f653aa98392adfb6444ff9a560ce1e94c8926
l0-o-0l <96638584+l0-o-0l@users.noreply.github.com>
2021-12-25 09:40:34 +0900

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,124 @@
+.logo {
+    width: 100px;
+    height: 40px;
+    margin-top: 14px;
+}
+
+h1 {
+    font-size: 45px;
+    margin-top: 30px;
+}
+
+.header {
+    display: flex;
+    max-width: px;
+    margin: 0 auto;
+    padding: 0 4%;
+    justify-content: space-between;
+    background-color: #ff4141;
+    background: #ff4141;
+    height: 120px;
+}
+
+.a {
+    background-size: cover;
+    background-position: center top;
+    background-repeat: no-repeat;
+}
+
+li {
+    background: #ffffff;
+    width: px;
+    height: px;
+    font-size: 18px;
+    margin-top: 20px;
+    background-color: #ff4141;
+}
+
+.b {
+    display: flex;
+    font-size: 18px;
+    text-transform: uppercase;
+    margin-top: 30px;
+    list-style: none;
+}
+
+.b li {
+    margin-left: 30px;
+}
+
+.b li a {
+    text-transform: none;
+    color: #1a1a1a;
+    text-decoration: none;
+}
+
+.c {
+    text-align: center;
+    justify-content: center;
+}
+
+.c p {
+    font-size: 40px;
+}
+
+.d {
+    font-size: 32px;
+    font-weight: normal;
+    background: #ffffff;
+}
+
+.button {
+    font-size: 18px;
+    background-color: #3d9b20;
+    color: #fff;
+    border-radius: 5px;
+    padding: 18px 32px;
+    text-decoration: none;
+}
+
+#home {
+    background: #ff4141;
+    width: px;
+    height: 100px;
+    background-color: #fffff;
+    min-height: vh;
+}
+
+.c h2 {
+    font-size: 70px;
+    top: 80px;
+}
+
+#home .d {
+    text-transform: none;
+}
+
+.e {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#x {
+    padding-top: 30px;
+    margin-top: -30px;
+}
+
+CSS #y {
+    padding-top: 350px;
+    margin-top: -350px;
+}
+
+CSS .o {
+    margin: 10px;
+}
+
+.r {
+    margin: 10px;
+}
+
+.p {
+    margin: 10px;
+}
